6e189052c4da6afd69a619e81013ad311ef09374
Caleb Buxton <me@cpb.ca>
2026-06-07 13:53:02 -0700

    :gear: VCR ignore 127.0.0.1 requests to prevent Selenium interception

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,13 +4,15 @@ require "rails/test_help"
 require "vcr"
 require "webmock"
 
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!(allow_localhost: [ "127.0.0.1", "localhost" ])
 
 VCR.configure do |config|
   config.cassette_library_dir = "test/cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data("<OLLAMA_API_BASE>") { RubyLLM.config.ollama_api_base }
-  config.ignore_localhost = true
+  config.ignore_request do |request|
+    URI(request.uri).host == "127.0.0.1"
+  end
 end
 
 module ActiveSupport
