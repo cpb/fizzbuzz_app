1e2343059d493cfe7e8eae2054fbe9b0bba00de1
Caleb Buxton <me@cpb.ca>
2026-06-07 08:41:25 -0700

    🔥 remove manual check of pre-commit and ci

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,4 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
-
-  private 
-    def foo
-      return true
-    end
 end
