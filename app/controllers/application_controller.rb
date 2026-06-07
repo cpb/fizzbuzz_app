d5725d3548d295483951aefd07e4703c80a18327
Caleb Buxton <me@cpb.ca>
2026-06-07 08:35:13 -0700

    💥 check ci runs lint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private 
+    def foo
+      return true
+    end
 end
