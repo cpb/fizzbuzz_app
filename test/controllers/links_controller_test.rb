a97624f4bde097267ae3fb777f3259fa8c50f246
Caleb Buxton <me@cpb.ca>
2026-06-07 16:57:39 -0700

    test: links-controller — failing test for hill

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -1,10 +1,9 @@
 require "test_helper"
 
 class LinksControllerTest < ActionDispatch::IntegrationTest
-  test "POST /links/publish enqueues PublishGistJob" do
-    link = links(:one)
-    assert_enqueued_with(job: PublishGistJob, args: [ { link_id: link.id, session_id: "" } ]) do
-      post links_publish_url(id: link.id)
+  test "POST /links/publish enqueues PublishGistJob without a link_id" do
+    assert_enqueued_with(job: PublishGistJob, args: [ {} ]) do
+      post links_publish_url
     end
     assert_response :no_content
   end
